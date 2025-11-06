608c5257b589c6135804fd0d26c19600266ad7ed
musulmankul02 <musulmankulkadyrbekuulu@gmail.com>
2025-11-06 19:27:22 +0600

    com

--- app/product/urls.py
+++ app/product/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app.product.views import ProductAPI, ToggleFavoriteViewSet, FavoriteListViewSet, OrderViewSet, OrderCreateViewSet
+from app.product.views import ProductAPI, ContactAPI
 
 router = DefaultRouter()
 router.register(r"product", ProductAPI, basename='product')
-router.register(r'favorites/toggle', ToggleFavoriteViewSet, basename='toggle-favorite')
-router.register(r'favorites', FavoriteListViewSet, basename='favorite')
-router.register(r'orders', OrderViewSet, basename='orders')
-router.register(r'orders/create', OrderCreateViewSet, basename='order-create')
+router.register(r"contact", ContactAPI, basename='contact')
+
 
 urlpatterns = [
     path("", include(router.urls))
